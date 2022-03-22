d0898d0574b02bb28e120cd1039d3595eae3e5e8
HKunayn <102166198+HKunayn@users.noreply.github.com>
2022-03-22 17:42:09 +0300

    Create Values.java

---
+++ Values.java
@@ -0,0 +1,30 @@
+
+public interface Values {
+	final String upper = "------------------------------------( TeleponeBookSystem build 2 )------------------------------------";
+	final String lower = "------------------------------------------------------------------------------------------------------";
+	final String firstName = "first name";
+	final String lastName = "last name";
+	final String phone = "phone number";
+	final String city = "city";
+	final String address = "address";
+	final String sex = "sex";
+	final String email = "Email";
+	final String dataNemes[] = {firstName,lastName,phone,city,address,sex,email};
+	final String dataNemesfixed[] = {"first name     ","last name      ","phone number   ","city           ","address        ","sex            ","Email          "};
+	final String dataNemesfixed2 = "first name     last name      phone number   city           address        sex            Email          ";
+	final String welcomeMSG = "welcome to the program, enter any number from 1 to 7";
+	final String mainMSG = "enter any number from 1 to 7";
+	final String thanksMSG = "thank you for using the program!";
+	final String shutDownMSG = "program shutdown!";
+	final String exitMSG = "Are you sure you want to exit ?";
+	final String deletedMSG = "all the data will be deleted!";
+	final String twoOptionsMSG = "1:yes\t2:no";
+	final String fiveOptionsMSG = "1:add\t2:remove\t3:update\t4:Display\t5:Search\t6:sort\t7:Exit";
+	final String enterOptionMSG = "1:Type Enter to back to mainMenu ";
+	final String foundedMSG = "founded Successfully , ";
+	final String notFoundedMSG = " can not be founded";
+	final String searchMSG = "search a person's phone number by his/her first name";
+	final String removeMSG = "remove a person's by his/her phone number";
+	final String updateMSG = "update phone number any person by type his/her first name";
+	final String updatedMSG = "'s phone number updated!";
+}
